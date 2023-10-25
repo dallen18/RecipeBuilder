b72ae3952f71a37c91259e8b3e962c48ede3f5b1
dallen18 <69290063+dallen18@users.noreply.github.com>
2023-10-25 01:32:11 -0500

    More changes to search display

--- app/assets/scripts/search-api.js
+++ app/assets/scripts/search-api.js
@@ -1,39 +1,52 @@
-window.onload(fetchRecipe());
+window.onload = () => {
+    // Initial recipe loading
+    fetchRecipe();
+};
 
 function checkInput() {
-    let recipeInput = document.getElementById('search-bar').value;
+    const recipeInput = document.getElementById('search-bar').value;
     if (recipeInput.length === 0) {
         alert("Please enter a recipe name");
     }
 }
 
-document.getElementById('search-btn').addEventListener("click", function (event) {
+
+const loadMoreButton = document.getElementById("load-more-btn");
+
+loadMoreButton.addEventListener("click", function (event) {
     event.preventDefault();
+    $('#recipe-results').empty();
+    window.scrollTo(0, 0);
     fetchRecipe();
 });
 
+document.getElementById('search-btn').addEventListener("click", function (event) {
+    event.preventDefault();
+    $('#recipe-results').empty();
+    fetchRecipe();
+});
+
+let page = 1; // Track the current page
+const resultsPerPage = 36; // Number of results per page
+
 async function fetchRecipe() {
-    let recipeQuery = document.getElementById('search-bar').value;
-    const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${recipeQuery}&instructionsRequired=true&fillIngredients=false&addRecipeInformation=true&ignorePantry=true&limitLicense=false&ranking=2&number=40`;
+    const recipeQuery = document.getElementById('search-bar').value;
+    const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${recipeQuery}&instructionsRequired=true&fillIngredients=false&addRecipeInformation=true&ignorePantry=true&limitLicense=false&ranking=2&number=${resultsPerPage}&offset=${(page - 1) * resultsPerPage}`;
 
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'a2eb59a3a5msha082d86bf50568bp10e897jsn2845f3101d32',
-            'X-RapidAPI-Host': `spoonacular-recipe-food-nutrition-v1.p.rapidapi.com`,
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
         },
     };
 
     try {
-
         const response = await fetch(url, options);
         const result = await response.json();
 
         console.log(result);
 
-        const image = result.results[0].image;
-
-        //Need to make responsive
         let container = document.createElement("div");
         container.className = "recipe-container";
         container.style.display = "grid";
@@ -41,24 +54,27 @@ async function fetchRecipe() {
         container.style.columnGap = "10px";
         container.style.rowGap = "15px";
         container.style.margin = "25px";
+        container.style.boxShadow = "6px 5px 5px 6px #fc1c1c";
 
         let placeholder = document.querySelector("#recipe-results");
         let out = "";
         for (let recipe of result.results) {
             out += `
-    <div class="recipe-item" style= "justify-content: center">
-        <div style="justify-content: center">
-            <img src="${recipe.image}">
-        </div> 
-        <div>
-            <div style="margin 5px;">${recipe.dishTypes[0]}</div>
-            <p><img src="../img/red-timer.png" id="timer" style="height: 20px; width: 20px; margin: 2px;"> ${recipe.readyInMinutes} Mins</p>
+        <div class="recipe-item" style="justify-content: center">
+            <div>
+                <img src="${recipe.image}">
+            </div> 
+            <div>
+                <div style="margin: 5px;">${recipe.dishTypes[0]}</div>
+                <p><img src="../img/red-timer.png" id="timer" style="height: 20px; width: 20px; margin: 5px;"> ${recipe.readyInMinutes} Mins</p>
+            </div>
         </div>
-    </div>
-    `;
+        `;
         }
-        container.innerHTML = out; // Set the container's HTML content
+        container.innerHTML = out;
         placeholder.appendChild(container);
+
+        page++; // Increment the page for the next fetch
 
     } catch (error) {
         console.error(error);
